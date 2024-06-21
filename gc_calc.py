71f4c7a81911b274b498218043f64136e5d8ce8d
Alex <alexandra.bekoy@gmail.com>
2024-06-21 13:04:42 +0300

    Comment info

--- gc_calc.py
+++ gc_calc.py
@@ -7,9 +7,15 @@ plt.style.use('ggplot')
 
 # ===================================================== FUNCTIONS =================================================================
 def calculate_gc(sequence):
+    ''' Simple function that calculates GC content of a single sequence and rounds the floating point number to the third decimal place.
+    '''
     print(round(gc_fraction(sequence), 3))
 
+
 def multiple_gc(seq_choice):
+    ''' Function that takes in multiple sequences as input from user and calculates GC content. Returns tuple of a list of the sequences
+    and a list of the GC contents of the sequences.
+    '''
     sequences = []
     gc_list = []
     print("All sequences must be of the same length.")
@@ -19,7 +25,17 @@ def multiple_gc(seq_choice):
         gc_list.append(round(gc_fraction(sequence), 3))
     return sequences , gc_list
 
+
 def calculate_window_gc(sequence, window, step):
+    '''Function that calculates the GC content of a single sequence with the sliding window method. The window and step size are passed in
+    as positional arguments and the user defines their values accordingly. Returns a tuple of a list of the windows the sequence has been 
+    split to and a list with the respective gc values of every window analyzed.
+
+        [Sliding window analysis (SWAN) is a commonly used method for studying the properties of molecular sequences:
+        data are plotted as moving averages of a particular criterion, such as the number of nucleotide changes,
+        for a window of a certain length slid along a sequence or sequence alignment (Tajima, 1991).
+        SWAN developed to reveal patterns of variability along nucleotide sequences.]
+    '''
     gc_parts = []
     window_count = 0
     window_list = []
@@ -31,7 +47,24 @@ def calculate_window_gc(sequence, window, step):
     return window_list, gc_parts
 
 
+def create_window_plot(sequence):
+    '''Function that takes plots the SWAN calculated content of a single sequence passed in by the user.
+    '''
+    x_axis, y_axis = calculate_window_gc(sequence, window_size, step_size)
+    plt.plot(x_axis, y_axis, color = "k", linestyle = "--", marker = "o", label = f"{sequence}")
+    plt.title("Sliding Window GC Content Calculation")
+    plt.xlabel("Sequence Windows")
+    plt.ylabel("GC content")
+    plt.legend()
+    plt.tight_layout()
+    plt.show()
+
+
 def multi_window_plot(x_windows, seq_dict):
+    '''Function that plots the SWAN calculated GC content of multiple sequences passed in by the user. Window and step size are the same
+    for all the sequences analyzed and they are defined by the user. The function takes in as an argument a dictionary with the sequences 
+    as keys and a list of GC values of each window as values.
+    '''
     colors_list = ['b', 'g', 'm','r', 'c', 'y', 'k']
     for num, (key, value) in enumerate(seq_dict.items()):
         color = colors_list[num % len(colors_list)]
@@ -42,18 +75,11 @@ def multi_window_plot(x_windows, seq_dict):
     plt.legend()
     plt.tight_layout()
     plt.show()
-     
-def create_window_plot(sequence):
-    x_axis, y_axis = calculate_window_gc(sequence, window_size, step_size)
-    plt.plot(x_axis, y_axis, color = "k", linestyle = "--", marker = "o", label = f"{sequence}")
-    plt.title("Sliding Window GC Content Calculation")
-    plt.xlabel("Sequence Windows")
-    plt.ylabel("GC content")
-    plt.legend()
-    plt.tight_layout()
-    plt.show()
-    
+
+
 def create_GC_barplot(x, y):
+    '''Function that creates barplot of the GC value calculated (not with SWAN) of multiple sequences.
+    '''
     fig, ax = plt.subplots()
     bar_container = ax.bar(x, y, width = 0.4, color = '#C04000')
     ax.set_title("Sequence GC Content Analysis")
@@ -104,7 +130,7 @@ else:
             window , gc_content = calculate_window_gc(seq, window_size, step_size)
             windows.append(window)
             gc_content_list.append(gc_content)
-        # create dictionary from sequences and gc_values
+        # create dictionary of sequences and gc_values
         pairs = zip(sequence_list, gc_content_list)
         seq_dict = dict(pairs)
         multi_window_plot(windows, seq_dict)
